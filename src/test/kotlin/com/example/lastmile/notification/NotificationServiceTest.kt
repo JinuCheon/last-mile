1929bea1e08510cf0409314e4568f0fc7830fc40
JinuCheon <woojin8787@gmail.com>
2024-12-08 22:25:06 +0900

    impl test first: build fail

--- src/test/kotlin/com/example/lastmile/notification/NotificationServiceTest.kt
+++ src/test/kotlin/com/example/lastmile/notification/NotificationServiceTest.kt
@@ -1,11 +1,21 @@
 package com.example.lastmile.notification
 
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.*
 
 class NotificationServiceTest {
 
     @Test
     fun `배송완료 이벤트가 발생하면 push notification 을 전송한다`() {
-        
+        // given
+        val notificationClient = mock<NotificationClient>()
+        val notificationService = NotificationService(notificationClient)
+        val deliveryId = 1L
+
+        // when
+        notificationService.sendDeliveryCompleteNotification(deliveryId)
+
+        // then
+        verify(notificationClient, times(1)).send(deliveryId)
     }
 }
