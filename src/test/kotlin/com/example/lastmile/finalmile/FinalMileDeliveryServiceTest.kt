ddb7d0987273079f0be5be5f0c321cc18b30b313
JinuCheon <woojin8787@gmail.com>
2024-12-08 17:08:49 +0900

    r 필드 밖으로 빼기. ??? 이게 왜 되는거지

--- src/test/kotlin/com/example/lastmile/finalmile/FinalMileDeliveryServiceTest.kt
+++ src/test/kotlin/com/example/lastmile/finalmile/FinalMileDeliveryServiceTest.kt
@@ -3,13 +3,12 @@ package com.example.lastmile.finalmile
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
+val repository = DeliveryMemoryRepository()
+val sut = FinalMileDeliveryService(repository)
+
 class FinalMileDeliveryServiceTest : StringSpec({
-
-    val repository = DeliveryMemoryRepository()
-    val sut = FinalMileDeliveryService(repository)
-
     "고객의 집 앞에 배송이 완료된 경우, Delivery 의 status 를 배송완료 상태로 변경한다." {
-        val deliveryId = createDelivery(repository)
+        val deliveryId = createDelivery()
 
         sut.complete(deliveryId)
 
@@ -18,7 +17,7 @@ class FinalMileDeliveryServiceTest : StringSpec({
     }
 })
 
-private fun createDelivery(repository: DeliveryMemoryRepository): Long {
+private fun createDelivery(): Long {
     val deliveryId = 1L
     repository.save(Delivery(deliveryId, DeliveryStatus.PICKED_UP))
     return deliveryId
