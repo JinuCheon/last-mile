05c5ef3d8cf5aabf41ccc73eac46acb716b2434f
JinuCheon <woojin8787@gmail.com>
2024-12-08 22:22:16 +0900

    add test

---
+++ src/test/kotlin/com/example/lastmile/notification/NotificationServiceTest.kt
@@ -0,0 +1,11 @@
+package com.example.lastmile.notification
+
+import org.junit.jupiter.api.Test
+
+class NotificationServiceTest {
+
+    @Test
+    fun `배송완료 이벤트가 발생하면 push notification 을 전송한다`() {
+        
+    }
+}
